bb2fc86117ab24ef1fe619390bfc380374686134
antony999k <amorales@procesar.com.mx>
2021-07-14 00:20:47 -0500

    :sparkles: Pausa y game over

--- src/Codigos/Globales/Manager.gd
+++ src/Codigos/Globales/Manager.gd
@@ -1,23 +1,40 @@
 extends Node
 
-enum EstadoJuego {INACTIVO, EN_JUEGO, JUEGO_TERMINADO}
+enum EstadoJuego {INACTIVO, EN_JUEGO, JUEGO_PERDIDO, JUEGO_TERMINADO}
 # Varialbes del juego
 var estadoJuegoActual = EstadoJuego.INACTIVO
 var enPausa:bool = false
-var empaparruchometro: int = 30;
+var empaparruchometro: int = 10;
 var figurasVerdaderas: Array = [] #{'tipo':valorFigura, 'objeto': figura}
 var eliminandoNoticias: bool = true # si es false esta compartiendo
 
 #signals
 signal s_desempaparruchar
 signal s_empaparruchar
+signal s_terminarNivel
 
 func _ready():
 	pause_mode = PAUSE_MODE_PROCESS
 
 func _input(event):
 	if event.is_action_pressed("Pausa"):
-		get_tree().paused = !get_tree().paused
+		if(get_tree().paused):
+			get_tree().get_root().get_node("ZonaJuego/Pausa").hide()
+			get_tree().paused = false
+		else:
+			get_tree().get_root().get_node("ZonaJuego/Pausa").show()
+			get_tree().paused = true
+
+func _process(delta):
+	if(estadoJuegoActual == EstadoJuego.EN_JUEGO):
+		if(empaparruchometro <= 0):
+			estadoJuegoActual = EstadoJuego.JUEGO_TERMINADO
+			get_tree().get_root().get_node("ZonaJuego/GameOver").show()
+			emit_signal("s_terminarNivel")
+		elif(empaparruchometro >= 20):
+			estadoJuegoActual = EstadoJuego.JUEGO_PERDIDO
+			get_tree().get_root().get_node("ZonaJuego/GameOver").show()
+			emit_signal("s_terminarNivel")
 
 func empaparruchar(cantidad=1):
 	Manager.empaparruchometro += cantidad
