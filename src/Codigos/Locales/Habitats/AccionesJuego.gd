17209c1cc1e97006a924153932140fce72b4fab6
antony999k <amorales@procesar.com.mx>
2021-07-14 23:39:51 -0500

    :sparkles: cosas bonitas

--- src/Codigos/Locales/Habitats/AccionesJuego.gd
+++ src/Codigos/Locales/Habitats/AccionesJuego.gd
@@ -4,13 +4,13 @@ onready var seleccion = $Seleccion
 onready var eliminar = $Eliminar
 onready var compartir = $Compartir
 
-
 func _input(event):
 	if event.is_action_pressed("Change"):
 		Manager.eliminandoNoticias = !Manager.eliminandoNoticias
-		
 		if(Manager.eliminandoNoticias):
-			seleccion.rect_position.y = 719
+			seleccion.rect_position = Vector2(compartir.rect_position.x-90,compartir.rect_position.y-90)
 		else:
-			seleccion.rect_position.y = 513
+			seleccion.rect_position = Vector2(eliminar.rect_position.x-60,eliminar.rect_position.y-40)
+	
+	
 
--- src/Codigos/Locales/Interfaz/FondoIzqResponsivo.gd
+++ src/Codigos/Locales/Interfaz/FondoIzqResponsivo.gd
@@ -1,6 +1,6 @@
-extends ColorRect
+extends Control
 
-var sizeX:int = 300;
+var sizeX:int = 600;
 var lastWidth:int = 0;
 
 func _ready():
@@ -14,3 +14,7 @@ func changeWidth():
 	sizeX = get_viewport().size.x * Constants.playableArea 
 	set_size(Vector2(sizeX, get_viewport().size.y))
 	lastWidth = get_viewport().size.x
+	
+	get_node("Randal").position.x = sizeX/2
+	get_node("Slider").position.x = sizeX/2
+	get_node("Alex").position.x = sizeX/2
