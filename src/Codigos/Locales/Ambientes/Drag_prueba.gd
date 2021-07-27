35e676a48dd074248b852e0558c2f5c707dc634c
unknown <lcolchadom@centro.edu.mx>
2021-07-26 21:53:29 -0500

    agrego drag and drop, particulas y senales

---
+++ src/Codigos/Locales/Ambientes/Drag_prueba.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var estrellas = preload("res://Escenas/Objetos/Estrellitas.tscn")
+var material_estrella = preload("res://Recursos/Visuales/Materiales/estrellitas_material.tres")
+
+func _ready():
+	Senales.connect("cambia_paparruchometro", self, '_particulas')
+	#_carga_particulas()
+	pass
+
+func _particulas(puntos):
+	var instance_particulas = estrellas.instance()
+	instance_particulas.position = $Basura.position
+	add_child(instance_particulas)
+	
+
+func _carga_particulas():
+	var particula_lag = Particles2D.new()
+	particula_lag.material = material_estrella
+	particula_lag.one_shot = true
+	particula_lag.emitting = true
+	add_child(particula_lag)
