71ba384bc4114116e2d7e1845d5e235db98511a1
antony999k <amorales@procesar.com.mx>
2021-07-28 00:43:32 -0500

    :sparkles: ventanas generator actualizado

--- src/Codigos/Locales/Objetos/VentanaWikis.gd
+++ src/Codigos/Locales/Objetos/VentanaWikis.gd
@@ -1,12 +1,12 @@
-extends RigidBody2D
+extends Sprite
 
 onready var colision = $CollisionShape2D
-onready var wiki = $Wiki
 	
 onready var audioClick = $AudioClick
 onready var clicBien = $ClicBien
 onready var clicMal = $ClicMal
 onready var tween = $Tween
+onready var figura = $Figura
 var valorFigura: int = -1
 var clickeada: bool = false
 var deleteada: bool = false
@@ -15,7 +15,7 @@ export var speed: float = 180;
 
 func _input(event):
 	if event is InputEventMouseButton && event.is_pressed() && event.button_index == BUTTON_LEFT:
-		if wiki.get_rect().has_point(to_local(event.position)) && !clickeada:
+		if get_rect().has_point(to_local(event.position)) && !clickeada:
 			clickeada=true
 			audioClick.play()
 			if(Helpers.existFigura(Manager.figurasVerdaderas, valorFigura)):
@@ -45,20 +45,19 @@ func _ready():
 	Constants.FIGURA.Estrella: load("res://Codigos/Locales/Objetos/Estrella.tres"),
 	Constants.FIGURA.CircM: load("res://Codigos/Locales/Objetos/CircM.tres")
 	}
-	var image = get_node("Wiki/Figura")
 	var rng = RandomNumberGenerator.new()
 	rng.randomize();
 	valorFigura = rng.randi_range(0,Constants.FIGURA.size() -1)
-	image.texture = figures_data[valorFigura].image
+	figura.texture = figures_data[valorFigura].image
 	#Escalar random
 	var nScale = rand_range(1.8,2.8)
-	wiki.scale = Vector2(nScale, nScale)
+	scale = Vector2(nScale, nScale)
 
 
 func _physics_process(delta):
 	if(!deleteada):
 		position.y += speed * delta
-		if(position.y > get_viewport().size.y + get_node("Wiki").texture.get_height()):
+		if(position.y > get_viewport().size.y + texture.get_height()):
 			deleteada = true;
 			Manager.emit_signal("s_afueraPantalla", position.x)
 			Manager.empaparruchar()
@@ -67,11 +66,12 @@ func _physics_process(delta):
 		
 
 func closeAnimation(tipo=0):
-	tween.interpolate_property(wiki, "scale",
-		wiki.scale, Vector2(0, 0), 0.5,
+	tween.interpolate_property(self, "scale",
+		scale, Vector2(0, 0), 0.5,
 		Tween.TRANS_BACK , Tween.EASE_IN)
 	tween.start()
 
 
 func _on_Tween_tween_completed(object, key):
 	queue_free()
+
